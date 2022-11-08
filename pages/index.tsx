26da33f9ddd5c08895461914afc1e4f3835e2e66
Zavier Miller <zavierjmiller@gmail.com>
2022-11-08 09:59:37 -0500

    add formatting and vscode settings

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import Head from 'next/head'
+import Image from 'next/image'
 
 export default function Home() {
   return (
@@ -61,5 +61,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  );
+  )
 }
