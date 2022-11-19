e7639f041399effa4625f6561a4f6f15b3ac5c4a
collins-self <collinsnater03@gmail.com>
2022-11-18 19:01:06 -0500

    fixed the display type of the drawer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
+import { Box } from '@mui/system'
 import type { AppProps } from 'next/app'
 import NavigationDrawer from '../components/Drawer'
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    <Box sx={{ display: 'flex' }}>
       <NavigationDrawer />
       <Component {...pageProps} />
-    </div>
+    </Box>
   )
 }
