a0895e229d541c88d1091770465888eb22464049
Zavier Miller <zavierjmiller@gmail.com>
2022-11-21 14:54:05 -0500

    create model and api types

---
+++ utils/types/models.ts
@@ -0,0 +1,34 @@
+export interface User {
+  _id?: string
+  name: string
+  email: string
+  imageUrl: string
+}
+
+export interface ItemDefinition {
+  _id?: string
+  name: string
+  category?: string | Category
+  attributes?: (string | Attribute)[]
+}
+
+export interface InventoryItem {
+  _id?: string
+  itemDefinition: string | ItemDefinition
+  attributeIds: string[]
+  attributeValues: string[]
+  quantity: number
+  assignee: string | User
+}
+
+export interface Category {
+  _id?: string
+  name: string
+}
+
+export interface Attribute {
+  _id?: string
+  name: string
+  possibleValues: 'test' | 'number' | string[]
+  color: string
+}
