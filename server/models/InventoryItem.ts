7311ff3cdde1cd00649783e1a9f32a5777695ea1
Zavier Miller <zavierjmiller@gmail.com>
2022-11-21 15:04:19 -0500

    add schema for inventory item in mongoose

---
+++ server/models/InventoryItem.ts
@@ -0,0 +1,34 @@
+import { model, Schema, Document, models, Model } from 'mongoose'
+import { InventoryItem } from '../../utils/types'
+
+const InventoryItemSchema = new Schema({
+  itemDefinition: {
+    type: { type: Schema.Types.ObjectId, ref: 'ItemDefinition' },
+    required: true,
+  },
+  attributeIds: {
+    type: [{ type: Schema.Types.ObjectId, ref: 'Attribute' }],
+    required: false,
+    default: [],
+  },
+  attributeValues: {
+    type: [String],
+    required: false,
+    default: [],
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+  assignee: {
+    type: { type: Schema.Types.ObjectId, ref: 'User' },
+    required: false,
+  },
+})
+
+export interface InventoryItemDocument
+  extends Omit<InventoryItem, '_id'>,
+    Document {}
+
+export default (models.InventoryItem as Model<InventoryItemDocument>) ||
+  model<InventoryItemDocument>('InventoryItem', InventoryItemSchema)
