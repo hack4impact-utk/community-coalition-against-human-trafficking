7cd9921785a79668d92ede063585876c3b8ccb01
Zavier Miller <zavierjmiller@gmail.com>
2022-11-21 14:55:08 -0500

    create itemdefintion schema in mongo

---
+++ server/models/ItemDefinition.ts
@@ -0,0 +1,38 @@
+import { model, Schema, Document, models, Model } from 'mongoose'
+import { ItemDefinition } from '../../utils/types'
+
+const ItemDefinitionSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type: { type: Schema.Types.ObjectId, ref: 'Category' },
+    required: false,
+  },
+  attributes: {
+    type: [{ type: Schema.Types.ObjectId, ref: 'Attribute' }],
+    required: false,
+    default: [],
+  },
+  internal: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+  lowStockThreshold: {
+    type: Number,
+    required: true,
+  },
+  criticalStockThreshold: {
+    type: Number,
+    required: true,
+  },
+})
+
+export interface ItemDefinitionDocument
+  extends Omit<ItemDefinition, '_id'>,
+    Document {}
+
+export default (models.ItemDefinition as Model<ItemDefinitionDocument>) ||
+  model<ItemDefinitionDocument>('ItemDefinition', ItemDefinitionSchema)
