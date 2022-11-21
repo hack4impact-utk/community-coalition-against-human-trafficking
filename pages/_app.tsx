de4f2655608d03b0aca63b8c0d95edeb6ed76417
Cole <jmcmaho9@vols.utk.edu>
2022-11-20 20:39:24 -0500

    CCAHT-12 Mui Theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
+import ThemeProvider from '@mui/material/styles/ThemeProvider'
 import type { AppProps } from 'next/app'
+import theme from '../utils/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
