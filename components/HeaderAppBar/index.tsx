087fac2c24b1037a27fe8ba3b41b5fd2f56d2c8d
YarDog4 <yarddoga@vols.utk.edu>
2022-11-30 22:01:11 -0500

    CCAHT-8-appbar resolved zavier's most recent com.

--- components/HeaderAppBar/index.tsx
+++ components/HeaderAppBar/index.tsx
@@ -21,7 +21,6 @@ interface HeaderAppBarProps {
 }
 
 export default function HeaderAppBar(props: HeaderAppBarProps) {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null)
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
     null
   )
@@ -30,17 +29,12 @@ export default function HeaderAppBar(props: HeaderAppBarProps) {
     setAnchorElUser(event.currentTarget)
   }
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null)
-  }
-
   const handleCloseUserMenu = () => {
     setAnchorElUser(null)
   }
 
   return (
-    <AppBar position="static" color="transparent">
-      <Container maxWidth="xl">
+    <AppBar position="static" color="transparent" sx={{px:2}}>
         <Toolbar disableGutters>
           <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
             <IconButton
@@ -49,27 +43,9 @@ export default function HeaderAppBar(props: HeaderAppBarProps) {
               aria-controls="menu-appbar"
               aria-haspopup="true"
               color="inherit"
+              onClick={() => props.setDrawerOpen(true)}
             >
-              <MenuIcon />
             </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            ></Menu>
           </Box>
           {/* keeps the sign in user circle in the right most corner */}
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}></Box>
@@ -102,7 +78,6 @@ export default function HeaderAppBar(props: HeaderAppBarProps) {
             </Menu>
           </Box>
         </Toolbar>
-      </Container>
     </AppBar>
   )
 }
