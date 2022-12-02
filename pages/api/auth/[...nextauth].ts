6d6ca25ce1d0fae3feef28a2761a5daaf22672eb
Rudra Patel <patelrudra2003@gmail.com>
2022-12-01 23:23:17 -0500

    Fixes next-auth type definitions

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,8 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
-export const authOptions = {
+import { Account, Profile } from 'next-auth'
+
+export default NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -11,11 +13,11 @@ export const authOptions = {
   callbacks: {
     async signIn({ account, profile }) {
       // Ensure that only users with the proper email domain may say in
-      if (account.provider == "google") {
-        return profile.email_verified && profile.email.endsWith("@gmail.com")
+      if (account?.provider === 'google') {
+        console.log(profile)
+        return profile?.email_verified && profile?.email?.endsWith('@gmail.com')
       }
       return true
     },
   },
-}
-export default NextAuth(authOptions)
+})
