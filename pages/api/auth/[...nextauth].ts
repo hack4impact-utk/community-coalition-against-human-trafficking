d6afad88f67f7051d9a4e74ab7c1e470325f3b29
Rudra Patel <patelrudra2003@gmail.com>
2022-12-01 23:37:14 -0500

    Removes console.log

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -13,7 +13,6 @@ export default NextAuth({
     async signIn({ account, profile }) {
       // Ensure that only users with the proper email domain may say in
       if (account?.provider === 'google') {
-        console.log(profile)
         return profile?.email_verified && profile?.email?.endsWith('@gmail.com')
       }
       return true
