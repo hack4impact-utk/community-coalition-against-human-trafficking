9892daddf1a52d2a3c15d54e9ec3aacd86f20869
Zavier Miller <zavierjmiller@gmail.com>
2022-12-05 16:29:45 -0500

    create server funcs for getting, updating and deleting

--- server/actions/User.ts
+++ server/actions/User.ts
@@ -2,8 +2,25 @@ import mongoDb from '../index'
 import UserSchema from '../models/User'
 import { User } from '../../utils/types'
 
+export async function getUser(id: number) {
+  await mongoDb()
+  return await UserSchema.findById(id)
+}
+
 export async function createUser(user: User) {
   await mongoDb()
 
   return await UserSchema.create(user)
+}
+
+export async function updateUser(id: number, user: User) {
+  await mongoDb()
+
+  return await UserSchema.findByIdAndUpdate(id, user)
+}
+
+export async function deleteUser(id: number) {
+  await mongoDb()
+
+  return await UserSchema.findByIdAndDelete(id)
 }
