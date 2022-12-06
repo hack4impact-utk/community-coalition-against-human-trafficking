36d940599a8bdc1efd4af1983e62a49a09e6dd73
YarDog4 <yarddoga@vols.utk.edu>
2022-12-05 20:06:20 -0500

    CCAHT-8- removed imports

--- components/HeaderAppBar/index.tsx
+++ components/HeaderAppBar/index.tsx
@@ -6,10 +6,8 @@ import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
 import Menu from '@mui/material/Menu'
 import MenuIcon from '@mui/icons-material/Menu'
-import Container from '@mui/material/Container'
 import Avatar from '@mui/material/Avatar'
 import MenuItem from '@mui/material/MenuItem'
-import { blue } from '@mui/material/colors'
 
 const settings = ['Sign out']
 
