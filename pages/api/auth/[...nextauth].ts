9951ff2f346bee7e6935916c51406c294b9dd56f
Zavier Miller <zavierjmiller@gmail.com>
2023-01-25 14:21:02 -0500

    use mongodb adapter to create user

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,7 +1,13 @@
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
+import { MongoClient } from 'mongodb'
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
+const client = new MongoClient(process.env.MONGODB_URI)
+const clientPromise = client.connect()
+
 export default NextAuth({
+  adapter: MongoDBAdapter(clientPromise),
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
