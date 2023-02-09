1929bcbd4e66385a5fcc534e5d34926e4a0feed8
Zavier Miller <zavierjmiller@gmail.com>
2023-02-09 10:03:21 -0500

    fix display issues with DefaultLayout

--- components/DefaultLayout/index.tsx
+++ components/DefaultLayout/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavigationDrawer from '../NavigationDrawer'
 import HeaderAppBar from '../HeaderAppBar'
+import Box from '@mui/material/Box'
 interface DefaultLayoutProps {
   children: React.ReactNode
 }
@@ -8,10 +9,18 @@ interface DefaultLayoutProps {
 export default function DefaultLayout({ children }: DefaultLayoutProps) {
   const [drawerOpen, setDrawerOpen] = React.useState(false)
   return (
-    <section className="layout">
+    <>
       <HeaderAppBar setDrawerOpen={setDrawerOpen} />
-      <NavigationDrawer setDrawerOpen={setDrawerOpen} open={drawerOpen} />
-      <main>{children}</main>
-    </section>
+      <Box
+        component="section"
+        className="layout"
+        sx={{ display: 'flex', p: 1 }}
+      >
+        <NavigationDrawer setDrawerOpen={setDrawerOpen} open={drawerOpen} />
+        <Box component="main" sx={{ flexGrow: 1 }}>
+          {children}
+        </Box>
+      </Box>
+    </>
   )
 }
