296ae5de98e58b3170dc7a86b80d09a6e9717941
Rudra Patel <patelrudra2003@gmail.com>
2023-02-19 23:46:53 -0500

    Updates test page

--- pages/queryParamsTest.tsx
+++ pages/queryParamsTest.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
 import { addURLQueryParam, removeURLQueryParam } from '../utils/queryParams'
+import { useRouterQuery } from '../utils/queryParams'
 
 export default function Test() {
   // IMPORTANT
@@ -11,25 +12,38 @@ export default function Test() {
   // This has to due with how nextjs router is made and timings/refreshing the page
   // The workaround is to wait for the router object itself to update in a useEffect
   // Here is how I would access the path in code -- I would just use a state variable
-  const router = useRouter()
-  const [path, setPath] = useState("")
-  useEffect(() => setPath(router.asPath), [router])
+  const {router, path} = useRouterQuery()
 
   // proof that this works:
   useEffect(() => console.log(path), [path])
 
-
   return (
     <>
-      <button onClick={() => addURLQueryParam(router, "hi1", "test")}>add key 1</button>
-      <button onClick={() => addURLQueryParam(router, "hi2", "test")}>add key 2</button>
-      <button onClick={() => addURLQueryParam(router, "hi3", "test")}>add key 3</button>
-      <button onClick={() => addURLQueryParam(router, "hi4", "test")}>add key 4</button>
-      <button onClick={() => addURLQueryParam(router, "hi5", "test")}>add key 5</button>
+      <button onClick={() => addURLQueryParam(router, 'hi1', 'test')}>
+        add key 1
+      </button>
+      <button onClick={() => addURLQueryParam(router, 'hi2', 'test')}>
+        add key 2
+      </button>
+      <button onClick={() => addURLQueryParam(router, 'hi3', 'test')}>
+        add key 3
+      </button>
+      <button onClick={() => addURLQueryParam(router, 'hi4', 'test')}>
+        add key 4
+      </button>
+      <button onClick={() => addURLQueryParam(router, 'hi5', 'test')}>
+        add key 5
+      </button>
 
-      <button onClick={() => removeURLQueryParam(router, "hi1")}>delete key 1</button>
-      <button onClick={() => removeURLQueryParam(router, "hi2")}>delete key 2</button>
-      <button onClick={() => removeURLQueryParam(router, "hi3")}>delete key 3</button>
+      <button onClick={() => removeURLQueryParam(router, 'hi1')}>
+        delete key 1
+      </button>
+      <button onClick={() => removeURLQueryParam(router, 'hi2')}>
+        delete key 2
+      </button>
+      <button onClick={() => removeURLQueryParam(router, 'hi3')}>
+        delete key 3
+      </button>
     </>
   )
 }
