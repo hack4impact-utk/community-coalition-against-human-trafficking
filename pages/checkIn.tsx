c33d824e2051fa53f07118929be9a9d7a9c3c5f0
Shashank Bandaru <shashanksr.bandaru@gmail.com>
2023-02-20 18:35:29 -0500

    Set up card layout and partial page layout

--- pages/checkIn.tsx
+++ pages/checkIn.tsx
@@ -1,5 +1,31 @@
+import CheckInOutForm from '../components/CheckInOutForm'
+import { Button, Card, CardContent, CardActions, Grid, Typography } from '@mui/material'
+
 export default function CheckInPage() {
   return (
-    <h1>Check In</h1>
+    <>
+      <Grid container direction='column' justifySelf='center'>
+        <Grid item alignSelf='flex-end'>
+          <Button variant='outlined'>Create New Item</Button> {/* How to align this properly */}
+        </Grid>
+        <Grid item alignSelf='center'>
+          <Card sx={{minWidth: 600}}>
+            <Grid container direction='column'>
+              <Grid item>
+                <CardContent>
+                  <Typography variant='h6'>Check In Items</Typography>
+                  <CheckInOutForm/> {/* What to do with props */}
+                </CardContent>
+              </Grid>
+              <Grid item alignSelf='flex-end'>
+                <CardActions>
+                  <Button variant='contained'>Check In</Button>
+                </CardActions>
+              </Grid>
+            </Grid>
+          </Card>
+        </Grid>
+      </Grid>
+    </>
   )
 }
