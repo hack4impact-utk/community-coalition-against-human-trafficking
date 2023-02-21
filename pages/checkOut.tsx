a4183e4db09ca196a450a1aa31091f23a86b7bf9
Rudra Patel <patelrudra2003@gmail.com>
2023-02-21 11:21:41 -0500

    Populates checkOut page

--- pages/checkOut.tsx
+++ pages/checkOut.tsx
@@ -1,5 +1,34 @@
+import CheckInOutForm from '../components/CheckInOutForm'
+import { Box, Button } from '@mui/material'
+import { Typography } from '@mui/material'
+
 export default function CheckOutPage() {
   return (
-    <h1>Check Out</h1>
+    <Box
+      sx={{
+        boxShadow: 1,
+        maxWidth: 'min(600px, 90%)',
+        marginInline: 'auto',
+        marginTop: '2rem',
+        padding: '2rem',
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
+      <Typography variant="h4" fontSize={24} sx={{ marginBottom: '1rem' }}>
+        Check out items
+      </Typography>
+
+      {/* TODO: Add props to CheckInOutForm */}
+      <CheckInOutForm
+        kioskMode={true}
+        users={[]}
+        itemDefinitions={[]}
+      ></CheckInOutForm>
+
+      <Button variant="contained" sx={{ alignSelf: 'end', marginTop: '1rem' }}>
+        Check out
+      </Button>
+    </Box>
   )
 }
