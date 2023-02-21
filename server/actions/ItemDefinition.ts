bdade32b36bbcbd3a21aa0eb3d2cc0ab8ea0fa72
Zavier Miller <zavierjmiller@gmail.com>
2023-02-21 16:11:21 -0500

    use aggregate queries in item def getters

--- server/actions/ItemDefinition.ts
+++ server/actions/ItemDefinition.ts
@@ -8,7 +8,29 @@ import { getEntities, getEntity } from './MongoDriver'
  * @returns The ItemDefinition given by the itemDefinition parameter
  */
 export async function getItemDefinition(id: string) {
-  return await getEntity(ItemDefinitionSchema, id)
+  return await getEntity(ItemDefinitionSchema, id, [
+    {
+      $lookup: {
+        from: 'categories',
+        localField: 'category',
+        foreignField: '_id',
+        as: 'category',
+      },
+    },
+    {
+      $lookup: {
+        from: 'attributes',
+        localField: 'attributes',
+        foreignField: '_id',
+        as: 'attributes',
+      },
+    },
+    {
+      $set: {
+        category: { $arrayElemAt: ['$category', 0] },
+      },
+    },
+  ])
 }
 
 /**
@@ -16,5 +38,27 @@ export async function getItemDefinition(id: string) {
  * @returns All itemDefinitions
  */
 export async function getItemDefinitions() {
-  return await getEntities(ItemDefinitionSchema)
+  return await getEntities(ItemDefinitionSchema, [
+    {
+      $lookup: {
+        from: 'categories',
+        localField: 'category',
+        foreignField: '_id',
+        as: 'category',
+      },
+    },
+    {
+      $lookup: {
+        from: 'attributes',
+        localField: 'attributes',
+        foreignField: '_id',
+        as: 'attributes',
+      },
+    },
+    {
+      $set: {
+        category: { $arrayElemAt: ['$category', 0] },
+      },
+    },
+  ])
 }
