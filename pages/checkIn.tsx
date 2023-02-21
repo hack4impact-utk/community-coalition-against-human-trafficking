3c0ac8867fbd79325d97797329adaa319a7b29c9
Shashank Bandaru <shashanksr.bandaru@gmail.com>
2023-02-20 19:50:02 -0500

    .

--- pages/checkIn.tsx
+++ pages/checkIn.tsx
@@ -1,31 +1,36 @@
 import CheckInOutForm from '../components/CheckInOutForm'
-import { Button, Card, CardContent, CardActions, Grid, Typography } from '@mui/material'
+import { Box, Button, Card, CardContent, CardActions, Grid, Typography } from '@mui/material'
 
 export default function CheckInPage() {
   return (
-    <>
-      <Grid container direction='column' justifySelf='center'>
-        <Grid item alignSelf='flex-end'>
-          <Button variant='outlined'>Create New Item</Button> {/* How to align this properly */}
-        </Grid>
-        <Grid item alignSelf='center'>
-          <Card sx={{minWidth: 600}}>
-            <Grid container direction='column'>
-              <Grid item>
-                <CardContent>
-                  <Typography variant='h6'>Check In Items</Typography>
-                  <CheckInOutForm/> {/* What to do with props */}
-                </CardContent>
-              </Grid>
-              <Grid item alignSelf='flex-end'>
-                <CardActions>
-                  <Button variant='contained'>Check In</Button>
-                </CardActions>
-              </Grid>
+    <Grid container direction='row' justifySelf='center' sx={{width: '80%'}}>
+
+      {/* Card and Grid for form and submit button. */}
+      <Grid item alignSelf='center'>
+        <Card>
+          <Grid container direction='column'>
+
+            <Grid item>
+              <CardContent>
+                <Typography variant='h6'>Check In Items</Typography>
+                <CheckInOutForm kioskMode={true} users={[]} itemDefinitions={[]}/>
+              </CardContent>
             </Grid>
-          </Card>
-        </Grid>
+
+            <Grid item alignSelf='flex-end'>
+              <CardActions>
+                <Button variant='contained'>Check In</Button>
+              </CardActions>
+            </Grid>
+
+          </Grid>
+        </Card>
       </Grid>
-    </>
+
+      <Grid item alignSelf='flex-start'>
+        <Button variant='outlined'>Create New Item</Button> {/* How to align this properly */}
+      </Grid>
+
+    </Grid>
   )
 }
