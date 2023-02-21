c891818f70af66ca8867b78f823c11a1e5016fe2
collins-self <collinsnater03@gmail.com>
2023-02-21 11:11:32 -0500

    commit

--- server/actions/InventoryItems.ts
+++ server/actions/InventoryItems.ts
@@ -6,7 +6,9 @@ import InventoryItemSchema from '../models/InventoryItem'
  * Gets all Items from the database
  * @returns ALL items
  */
-export async function getInventoryItem() {
+export async function getInventoryItems() {
   await mongoDb()
   return await InventoryItemSchema.find()
 }
+
+
