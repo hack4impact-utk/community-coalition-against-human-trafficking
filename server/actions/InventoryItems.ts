2319fc8f8c2fe6f6d1ff060fa61e6bae4065207c
collins-self <collinsnater03@gmail.com>
2023-02-23 17:43:25 -0500

    template for functions

--- server/actions/InventoryItems.ts
+++ server/actions/InventoryItems.ts
@@ -1,14 +1,34 @@
-import mongoDb from '../index'
-import InventoryItemSchema from '../models/InventoryItem'
-
+import InventoryItem from '../models/InventoryItem';
+import * as MongoDriver from '../actions/MongoDriver';
 
 /**
- * Gets all Items from the database
- * @returns ALL items
+ * Checks to see if an item is in the inventory and will add to the quantity or
+ * create the item if needed.
+ * @param item
+ * @param quantity of item
+ * @returns checksIn Items to the inventory
  */
-export async function getInventoryItems() {
-  await mongoDb()
-  return await InventoryItemSchema.find()
+export async function checkInInventoryItem(item: InventoryItem, quantityItem: number) {
+    if (MongoDriver.getEntities(item) = ) { // see what this returns when it finds something vs not
+            // add quantity
+    }
+    else {
+            // create new item
+    }
 }
 
-
+/**
+ * Checks to see if an item is in the inventory and will remove an amount or
+ * 400 error if it does not exist.
+ * @param item
+ * @param quantity of item to remove
+ * @returns checksIn Items to the inventory
+ */
+export async function checkOutInventoryItem(item: InventoryItem, quantityRemoved: number) {
+    if (MongoDriver.getEntities(item) = ) {// see what this returns when it finds something vs not
+            // add quantity
+    }
+    else {
+            // create new item
+    }
+}
