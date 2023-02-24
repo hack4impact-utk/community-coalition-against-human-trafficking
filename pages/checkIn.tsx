89ef05b38d6611023f01119275721bee79078c3d
Rudra Patel <patelrudra2003@gmail.com>
2023-02-23 20:16:19 -0500

    Updates checkIn page to match checkOut page

--- pages/checkIn.tsx
+++ pages/checkIn.tsx
@@ -6,44 +6,39 @@ import {
   CardContent,
   CardActions,
   Typography,
+  useMediaQuery,
 } from '@mui/material'
 
 export default function CheckInPage() {
+  const isMobileView = useMediaQuery('(max-width: 600px)')
   return (
     <Box
-      sx={{
-        display: 'flex',
-        flexFlow: 'row wrap',
-        justifyContent: 'center',
-        gap: '30px',
-      }}
+      mx="auto"
+      display="flex"
+      flexDirection="column"
+      maxWidth="min(600px, 80%)"
     >
-      <Card sx={{ width: '30%', marginTop: '50px' }}>
-        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+      <Button variant="outlined" sx={{ flexGrow: 0 }}>
+        Create new item
+      </Button>
+      <Card variant={isMobileView ? 'elevation' : 'outlined'} elevation={0}>
+        <Box px="1.5rem" py="2rem" display="flex" flexDirection="column">
           <CardContent>
-            <Typography variant="h5" sx={{ marginBottom: '30px' }}>
+            <Typography variant="h4" fontSize="1.5rem" mb="1rem">
               Check in items
             </Typography>
             <CheckInOutForm kioskMode={true} users={[]} itemDefinitions={[]} />
           </CardContent>
 
-          <CardActions sx={{ alignSelf: 'flex-end' }}>
-            <Button
-              variant="contained"
-              sx={{ marginBottom: '10px', marginRight: '10px' }}
-            >
-              Check In
+          <CardActions
+            sx={{ alignSelf: { xs: 'center', sm: 'end' }, pr: '16px' }}
+          >
+            <Button variant="contained" sx={{ mt: '-8px' }}>
+              Check in
             </Button>
           </CardActions>
         </Box>
       </Card>
-
-      <Button
-        variant="outlined"
-        sx={{ alignSelf: 'flex-start', marginTop: '30px' }}
-      >
-        Create New Item
-      </Button>
     </Box>
   )
 }
