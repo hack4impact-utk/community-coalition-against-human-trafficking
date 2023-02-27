0636709d3e81a46962fa4ebc90dc79d7c56e5030
Zavier Miller <zavierjmiller@gmail.com>
2023-02-27 16:23:56 -0500

    put autocomplete into CheckInOutForm

--- components/CheckInOutForm/index.tsx
+++ components/CheckInOutForm/index.tsx
@@ -2,21 +2,32 @@ import { Autocomplete, Box, FormControl, TextField } from '@mui/material'
 import { DateTimePicker } from '@mui/x-date-pickers'
 import dayjs, { Dayjs } from 'dayjs'
 import React from 'react'
-import { ItemDefinition, User } from 'utils/types'
+import { Attribute, AttributeRequest, ItemDefinition, User } from 'utils/types'
 import QuantityForm from 'components/CheckInOutForm/QuantityForm'
+import AttributeAutocomplete from 'components/AttributeAutocomplete'
+import { separateAttributes } from 'utils/attribute'
 
 interface Props {
   kioskMode: boolean
   users: User[]
   itemDefinitions: ItemDefinition[]
+  attributes?: Attribute[]
 }
 
-function CheckInOutForm({ kioskMode, users, itemDefinitions }: Props) {
+function CheckInOutForm({
+  kioskMode,
+  users,
+  itemDefinitions,
+  attributes,
+}: Props) {
   const [date, setDate] = React.useState<Dayjs | null>(dayjs(new Date()))
   const [quantity, setQuantity] = React.useState<number>(1)
   const [selectedStaff, setSelectedStaff] = React.useState<User | null>()
   const [selectedItemDefinition, setSelectedItemDefinition] =
     React.useState<ItemDefinition | null>()
+  const [selectedAttributes, setSelectedAttributes] =
+    React.useState<AttributeRequest>({})
+  const splitAttrs = separateAttributes(attributes)
 
   return (
     <FormControl fullWidth>
@@ -47,6 +58,13 @@ function CheckInOutForm({ kioskMode, users, itemDefinitions }: Props) {
           setSelectedItemDefinition(itemDefinition)
         }
         getOptionLabel={(itemDefinition) => itemDefinition.name}
+      />
+      <AttributeAutocomplete
+        attributes={splitAttrs.options}
+        sx={{ mt: 4 }}
+        onChange={(_e, attributes) => {
+          setSelectedAttributes(attributes)
+        }}
       />
       <QuantityForm quantity={quantity} setQuantity={setQuantity} />
     </FormControl>
