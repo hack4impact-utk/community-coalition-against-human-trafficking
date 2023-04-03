5b8b3a325ea708536e732d113bbce2519c38d98d
andlrutt <71574118+andlrutt@users.noreply.github.com>
2023-04-03 13:49:18 -0400

    init

--- pages/checkIn.tsx
+++ pages/checkIn.tsx
@@ -10,12 +10,65 @@ import {
   useMediaQuery,
   useTheme,
 } from '@mui/material'
-<<<<<<< HEAD
-=======
-import { CategoryResponse } from 'utils/types'
+import {
+  AttributeResponse,
+  CategoryResponse,
+  CheckInOutFormData,
+  InventoryItemAttributeRequest,
+  InventoryItemAttributeResponse,
+  InventoryItemRequest,
+  InventoryItemResponse,
+  ItemDefinitionResponse,
+} from 'utils/types'
 import { createResponse } from 'node-mocks-http'
 import categoriesHandler from '@api/categories'
 import { GetServerSidePropsContext, NextApiRequest } from 'next'
+import React from 'react'
+
+const realCategory: CategoryResponse = {
+  _id: '642892e2bacccd5ff01c4512',
+  name: 'Clothing',
+}
+
+const realAttributes: AttributeResponse[] = [
+  {
+    _id: '642892e3bacccd5ff01c451a',
+    name: 'Top Size',
+    possibleValues: ['Small', 'Medium', 'Large', 'Extra-Large'],
+    color: '#ebebeb',
+  },
+  {
+    _id: '642892e3bacccd5ff01c451b',
+    name: 'Shirt Type',
+    possibleValues: ['Short-Sleeve', 'Long-Sleeve'],
+    color: '#c7dbda',
+  },
+]
+
+const realInventoryItemAttributes: InventoryItemAttributeResponse[] = [
+  { attribute: { ...realAttributes[0] }, value: 'Medium' },
+  { attribute: { ...realAttributes[1] }, value: 'Long-Sleeve' },
+]
+
+const realItemDefinition: ItemDefinitionResponse = {
+  _id: '642892e3bacccd5ff01c4529',
+  name: 'Shirt',
+  category: {
+    _id: '642892e2bacccd5ff01c4512',
+    name: 'Clothing',
+  },
+  attributes: realAttributes,
+  internal: false,
+  lowStockThreshold: 20,
+  criticalStockThreshold: 10,
+}
+
+const realInventoryItem: InventoryItemResponse = {
+  _id: '642892e3bacccd5ff01c4549',
+  itemDefinition: realItemDefinition,
+  attributes: realInventoryItemAttributes,
+  quantity: 24,
+}
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const res = createResponse()
@@ -28,11 +81,39 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 interface Props {
   categories: CategoryResponse[]
 }
->>>>>>> 6a27a9d9a6059140b0588ce5e885e5e2ca54f7fd
 
 export default function CheckInPage({ categories }: Props) {
   const theme = useTheme()
   const isMobileView = useMediaQuery(theme.breakpoints.down('sm'))
+
+  const [formData, setFormData] = React.useState<CheckInOutFormData>(
+    {} as CheckInOutFormData
+  )
+
+  const onSubmit = async (formData: CheckInOutFormData) => {
+    const inventoryItem: Partial<InventoryItemRequest> = {
+      itemDefinition: formData.itemDefinition._id,
+      attributes: formData.attributes.map(
+        (attributeOption): InventoryItemAttributeRequest => ({
+          attribute: attributeOption.id,
+          value: attributeOption.value,
+        })
+      ),
+      assignee: formData.user?._id,
+    }
+
+    // TODO better way of coding URLs
+    await fetch(
+      `http://localhost:3000/api/inventoryItems/checkIn?quantity=${formData.quantityDelta}`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(inventoryItem),
+      }
+    )
+  }
 
   return (
     <Grid2 container sx={{ flexGrow: 1 }}>
@@ -65,18 +146,19 @@ export default function CheckInPage({ categories }: Props) {
               <CheckInOutForm
                 kioskMode={true}
                 users={[]}
-                itemDefinitions={[]}
+                itemDefinitions={[realItemDefinition]}
                 categories={categories}
-                onChange={(item) => {
-                  console.log(item)
-                }}
+                formData={formData}
+                setFormData={setFormData}
               />
             </CardContent>
 
             <CardActions
               sx={{ alignSelf: { xs: 'end' }, mt: { xs: 1, sm: 0 } }}
             >
-              <Button variant="contained">Check in</Button>
+              <Button onClick={() => onSubmit(formData)} variant="contained">
+                Check in
+              </Button>
             </CardActions>
           </Box>
         </Card>
