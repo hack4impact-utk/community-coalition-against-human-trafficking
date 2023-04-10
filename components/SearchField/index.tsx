29479e2634359ea36195ce99300626c4955c54c0
Zavier Miller <zavierjmiller@gmail.com>
2023-04-10 00:43:54 -0400

    fix bugs in search field

--- components/SearchField/index.tsx
+++ components/SearchField/index.tsx
@@ -1,25 +1,33 @@
 import { Search } from '@mui/icons-material'
 import { InputAdornment, OutlinedInput, TextField } from '@mui/material'
+import React from 'react'
 import { debounce } from 'ts-debounce'
-import { addURLQueryParam, useRouterQuery } from 'utils/queryParams'
+import {
+  addURLQueryParam,
+  removeURLQueryParam,
+  useRouterQuery,
+} from 'utils/queryParams'
 
 export default function SearchField() {
   const { router } = useRouterQuery()
 
   const onChange = (search: string) => {
-    addURLQueryParam(router, 'search', search)
+    if (!search) removeURLQueryParam(router, 'search')
+    else addURLQueryParam(router, 'search', search)
   }
   const debouncedOnChange = debounce(onChange, 500)
 
   return (
     <OutlinedInput
       placeholder="Search"
+      fullWidth
       endAdornment={
         <InputAdornment position="end">
           <Search />
         </InputAdornment>
       }
       onChange={(e) => debouncedOnChange(e.target.value)}
+      value={router.query.search || ''}
     ></OutlinedInput>
   )
 }
