286cb5694c226450f6295df67392cc27eaa3e02e
Zavier Miller <zavierjmiller@gmail.com>
2023-04-16 18:42:09 -0400

    rename data and remove comment

--- components/DesktopInventoryItemList/index.tsx
+++ components/DesktopInventoryItemList/index.tsx
@@ -292,7 +292,10 @@ export default function DesktopInventoryItemList(props: Props) {
           <TableBody>
             {visibleRows &&
               visibleRows.map((item) => (
-                <InventoryItemListItem inventoryItem={item} key={item._id} />
+                <InventoryItemListItem
+                  inventoryItemData={item}
+                  key={item._id}
+                />
               ))}
           </TableBody>
         </Table>
