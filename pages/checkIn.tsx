1c72e573a906a9747468759d8761aff348e8c9e1
Zavier Miller <zavierjmiller@gmail.com>
2023-04-16 19:11:36 -0400

    clean up

--- pages/checkIn.tsx
+++ pages/checkIn.tsx
@@ -89,16 +89,14 @@ export default function CheckInPage({
         smOffset={2}
         lgOffset={3}
       >
-        <DialogLink href={'/dialog/test'}>
-          <Button
-            variant="outlined"
-            fullWidth={isMobileView}
-            size="large"
-            sx={{ my: 2 }}
-          >
-            Create new item
-          </Button>
-        </DialogLink>
+        <Button
+          variant="outlined"
+          fullWidth={isMobileView}
+          size="large"
+          sx={{ my: 2 }}
+        >
+          Create new item
+        </Button>
       </Grid2>
 
       <Grid2 xs={12} sm={8} lg={6} smOffset={2} lgOffset={3}>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import { Button } from '@mui/material'
 import DialogLink from 'components/DialogLink'
-import { useAppDispatch, useAppSelector } from 'store'
-import { enableKioskMode, toggleKioskMode } from 'store/kiosk'
 
 export default function DashboardPage() {
   return (
