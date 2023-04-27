902886993a20b54ebb94d1338a31c391150d1b41
Shashank Bandaru <shashanksr.bandaru@gmail.com>
2023-04-27 17:00:14 -0400

    reset test page

--- pages/settings/items.tsx
+++ pages/settings/items.tsx
@@ -1,27 +1,5 @@
-import { Typography } from "@mui/material";
-import ItemDefinitionList from "components/ItemDefinitionList"
-import itemDefinitionsHandler from "@api/itemDefinitions"
-import { ItemDefinitionResponse } from "utils/types"
-import { apiWrapper } from "utils/apiWrappers"
-import { GetServerSidePropsContext } from 'next'
-
-interface Props {
-  itemDefinitions: ItemDefinitionResponse[]
-}
-
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  return {
-    props: {
-      itemDefinitions: await apiWrapper(itemDefinitionsHandler, context)
-    },
-  }
-}
-
-export default function ItemsPage({ itemDefinitions }: Props) {
+export default function ItemsPage() {
   return (
-    <>
-      <Typography variant='h4'>Items</Typography>
-      <ItemDefinitionList itemDefinitions={itemDefinitions} search="" />
-    </>
+    <h1>Items</h1>
   )
 }
