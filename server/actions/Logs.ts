cdcf4232f69131c95227b6c1bd84335952e672a7
Andrew Rutter <andlrutt@vols.utk.edu>
2023-04-26 21:55:54 -0400

    working database pipeline

--- server/actions/Logs.ts
+++ server/actions/Logs.ts
@@ -12,99 +12,115 @@ import { PipelineStage } from 'mongoose'
 const requestPipeline: PipelineStage[] = [
   {
     $lookup: {
-      from: 'itemDefinitions',
-      let: { itemDefinitions: '$itemDefinitions' },
+      from: 'inventoryItems',
+      let: { inventoryItems: '$inventoryItems' },
+      localField: 'item',
+      foreignField: '_id',
+      as: 'item',
       pipeline: [
         {
           $lookup: {
-            from: 'categories',
-            localField: 'category',
+            from: 'itemDefinitions',
+            let: { itemDefinitions: '$itemDefinitions' },
+            pipeline: [
+              {
+                $lookup: {
+                  from: 'categories',
+                  localField: 'category',
+                  foreignField: '_id',
+                  as: 'category',
+                },
+              },
+              {
+                $lookup: {
+                  from: 'attributes',
+                  localField: 'attributes',
+                  foreignField: '_id',
+                  as: 'attributes',
+                },
+              },
+              {
+                $set: {
+                  category: { $arrayElemAt: ['$category', 0] },
+                },
+              },
+            ],
+            localField: 'itemDefinition',
             foreignField: '_id',
-            as: 'category',
+            as: 'itemDefinition',
+          },
+        },
+        // itemDefinition was being returned as an array, so extract it from the array
+        {
+          $set: {
+            itemDefinition: { $arrayElemAt: ['$itemDefinition', 0] },
+          },
+        },
+        // creates a temporary array of attribute documents called 'attributeDocs' containing all relevant attribute documents
+        {
+          $lookup: {
+            from: 'attributes',
+            localField: 'attributes.attribute',
+            foreignField: '_id',
+            as: 'attributeDocs',
+          },
+        },
+        // the above lookup only gets the attribute docs from the 'attributes' collection.
+        // It does not generate the inventoryItem attribute/value pairs.
+        {
+          $addFields: {
+            // builds out the inventoryItem.attributes array from scratch
+            attributes: {
+              // for every attribute in inventoryItem.attributes object, create an attribute/value pair
+              $map: {
+                input: '$attributes',
+                as: 'attr', // attribute from the inventoryItem.attributes array
+                in: {
+                  attribute: {
+                    // find the corresponding document from the attributes collection with the same _id
+                    $arrayElemAt: [
+                      {
+                        $filter: {
+                          input: '$attributeDocs',
+                          as: 'doc', // document from the attributes collection
+                          cond: { $eq: ['$$doc._id', '$$attr.attribute'] },
+                        },
+                      },
+                      0,
+                    ],
+                  },
+                  // set the inventoryItem.attributes[i].value to its original value
+                  value: '$$attr.value',
+                },
+              },
+            },
+          },
+        },
+        // delete the temporary array of attribute documents
+        {
+          $project: {
+            attributeDocs: 0,
           },
         },
         {
           $lookup: {
-            from: 'attributes',
-            localField: 'attributes',
+            from: 'users',
+            localField: 'assignee',
             foreignField: '_id',
-            as: 'attributes',
+            as: 'assignee',
           },
         },
         {
           $set: {
-            category: { $arrayElemAt: ['$category', 0] },
+            assignee: { $arrayElemAt: ['$assignee', 0] },
           },
         },
       ],
-      localField: 'itemDefinition',
-      foreignField: '_id',
-      as: 'itemDefinition',
-    },
-  },
-  // itemDefinition was being returned as an array, so extract it from the array
-  {
-    $set: {
-      itemDefinition: { $arrayElemAt: ['$itemDefinition', 0] },
-    },
-  },
-  // creates a temporary array of attribute documents called 'attributeDocs' containing all relevant attribute documents
-  {
-    $lookup: {
-      from: 'attributes',
-      localField: 'attributes.attribute',
-      foreignField: '_id',
-      as: 'attributeDocs',
-    },
-  },
-  // the above lookup only gets the attribute docs from the 'attributes' collection.
-  // It does not generate the inventoryItem attribute/value pairs.
-  {
-    $addFields: {
-      // builds out the inventoryItem.attributes array from scratch
-      attributes: {
-        // for every attribute in inventoryItem.attributes object, create an attribute/value pair
-        $map: {
-          input: '$attributes',
-          as: 'attr', // attribute from the inventoryItem.attributes array
-          in: {
-            attribute: {
-              // find the corresponding document from the attributes collection with the same _id
-              $arrayElemAt: [
-                {
-                  $filter: {
-                    input: '$attributeDocs',
-                    as: 'doc', // document from the attributes collection
-                    cond: { $eq: ['$$doc._id', '$$attr.attribute'] },
-                  },
-                },
-                0,
-              ],
-            },
-            // set the inventoryItem.attributes[i].value to its original value
-            value: '$$attr.value',
-          },
-        },
-      },
-    },
-  },
-  // delete the temporary array of attribute documents
-  {
-    $project: {
-      attributeDocs: 0,
-    },
-  },
-  {
-    $lookup: {
-      from: 'users',
-      localField: 'staff',
-      foreignField: '_id',
-      as: 'staff',
     },
   },
   {
     $set: {
-      staff: { $arrayElemAt: ['$staff', 0] },
+      item: { $arrayElemAt: ['$item', 0] },
     },
   },
 ]
