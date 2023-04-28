25a92d5505a1e2ee19bc129f090e1ee458c10c0e
Zavier Miller <zavierjmiller@gmail.com>
2023-04-28 09:57:27 -0400

    clean up

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,18 @@
 import { Button } from '@mui/material'
 import DialogLink from 'components/DialogLink'
 import { useAppDispatch, useAppSelector } from 'store'
-import { toggleKioskMode } from 'store/kiosk';
+import { toggleKioskMode } from 'store/kiosk'
 
 export default function DashboardPage() {
-  const dispatch = useAppDispatch();
-  const kiosk = useAppSelector(state => state.kiosk)
-
-  console.log(kiosk)
+  const dispatch = useAppDispatch()
   return (
     <>
-    <DialogLink href={'/dialog/test'}>
-      <Button>Take me to dialog</Button>
-    </DialogLink>
-    <Button onClick={() => dispatch(toggleKioskMode())}>
-      Toggle Kiosk Mode
-    </Button>
+      <DialogLink href={'/dialog/test'}>
+        <Button>Take me to dialog</Button>
+      </DialogLink>
+      <Button onClick={() => dispatch(toggleKioskMode())}>
+        Toggle Kiosk Mode
+      </Button>
     </>
   )
 }
