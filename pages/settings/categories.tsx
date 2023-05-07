d087d3be3a42338b1ba13b2db511dbdd5e7ea91c
collins-self <collinsnater03@gmail.com>
2023-05-07 15:52:12 -0400

    populated category settings page

--- pages/settings/categories.tsx
+++ pages/settings/categories.tsx
@@ -1,5 +1,10 @@
 import categoriesHandler from '@api/categories'
+import { Button, Typography } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add'
+import Grid2 from '@mui/material/Unstable_Grid2'
 import CategoryList from 'components/CategoryList'
+import DialogLink from 'components/DialogLink'
+import SearchField from 'components/SearchField'
 import { GetServerSidePropsContext } from 'next'
 import { apiWrapper } from 'utils/apiWrappers'
 import { CategoryResponse } from 'utils/types'
@@ -17,8 +22,28 @@ interface CategoriesPageProps {
 export default function CategoriesPage({ categories }: CategoriesPageProps) {
   return (
     <>
-      <h1>Categories</h1>
-      <CategoryList categories={categories} search={''} />
+      <Grid2 container my={2} sx={{ flexGrow: 1, px: 2 }} gap={2}>
+        <Grid2 xs={12} container direction={'row'}>
+          <Typography variant="h5" sx={{ mb: 2, ml: 2 }}>
+            Categories
+          </Typography>
+          <Grid2 ml="auto" mr={6}>
+            <DialogLink href="/items/new" backHref="/settings/items">
+              <Button
+                variant="outlined"
+                startIcon={<AddIcon />}
+                sx={{ width: '100%' }}
+              >
+                Create New Category
+              </Button>
+            </DialogLink>
+          </Grid2>
+        </Grid2>
+        <Grid2 xs={12} md={5} lg={4} sx={{ px: 2 }}>
+          <SearchField />
+        </Grid2>
+        <CategoryList categories={categories} search={''} />
+      </Grid2>
     </>
   )
 }
