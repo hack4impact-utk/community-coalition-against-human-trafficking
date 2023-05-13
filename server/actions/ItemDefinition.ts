0d20d06af16088b9257f929f1e78347078179b54
Andrew Rutter <andlrutt@vols.utk.edu>
2023-05-13 03:47:42 -0400

    server func

--- server/actions/ItemDefinition.ts
+++ server/actions/ItemDefinition.ts
@@ -36,6 +36,43 @@ const softDeleteRequestPipeline: PipelineStage[] = [
   },
 ]
 
+const presentItemDefinitionsPipeline: PipelineStage[] = [
+  // pull in all inventory items fpr each item definition
+  {
+    $lookup: {
+      from: 'inventoryItems',
+      localField: '_id',
+      foreignField: 'itemDefinition',
+      as: 'inventoryItems',
+    },
+  },
+
+  // filter out inventory items with quantity 0
+  {
+    $set: {
+      inventoryItems: {
+        $filter: {
+          input: '$inventoryItems',
+          as: 'item',
+          cond: { $gt: ['$$item.quantity', 0] },
+        },
+      },
+    },
+  },
+
+  // at this point, each document (itemDefinition) has an array called "inventoryItems"
+  // that contains all inventory items that have that itemDefinition
+
+  // filter out item definitions where Document.inventoryItems.length === 0
+  // this indicates that there are no inventory items with quantity > 0 in the warehouse
+  // with the given itemDefinitionId
+  {
+    $match: {
+      inventoryItems: { $ne: [] },
+    },
+  },
+]
+
 /**
  * Finds an itemDefinition by its id
  * @id The id of the ItemDefinition object to find
@@ -57,5 +94,15 @@ export async function getItemDefinitions() {
   return (await getEntities(
     ItemDefinitionSchema,
     softDeleteRequestPipeline
+  )) as ItemDefinitionResponse[]
+}
+
+/**
+ * Finds all itemDefinitions that do not have the softDelete flag
+ * @returns All itemDefinitions that do not have the softDelete flag
+ */
+export async function getPresentItemDefinitions() {
+  return (await ItemDefinitionSchema.aggregate(
+    presentItemDefinitionsPipeline
   )) as ItemDefinitionResponse[]
 }
