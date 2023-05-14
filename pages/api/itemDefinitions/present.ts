9d73087222b0308d3edcabe24fa8b3ff6f4fcdab
Andrew Rutter <andlrutt@vols.utk.edu>
2023-05-13 22:09:59 -0400

    rename

--- pages/api/itemDefinitions/present.ts
+++ pages/api/itemDefinitions/present.ts
@@ -4,9 +4,8 @@ import { serverAuth } from 'utils/auth'
 import { getPresentItemDefinitions } from 'server/actions/ItemDefinition'
 import { errors } from 'utils/constants/errors'
 
-// @route GET api/itemDefintions - Returns a list of all itemDefintions in the database - Private
-// @route POST /api/itemDefintions - Create a itemDefinition from request body - Private
-export default async function itemDefinitionsHandler(
+// @route GET api/itemDefintions/present - Returns a list of all itemDefintions in the database that have some stock in the warehouse - Private
+export default async function presentItemDefinitionsHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
