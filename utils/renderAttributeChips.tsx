b4a0e0e27ad73865e6fb2073c14189b85dc8d1d8
Zavier Miller <zavierjmiller@gmail.com>
2023-05-16 15:14:16 -0400

    wip

--- utils/renderAttributeChips.tsx
+++ utils/renderAttributeChips.tsx
@@ -1,11 +1,62 @@
 import { Chip } from '@mui/material'
+import { useAppSelector } from 'store'
 import getContrastYIQ from './getContrastYIQ'
 import { InventoryItemAttributeResponse } from './types'
 
-const renderAttributeChips = (
+const attributeComparator = (
+  a: InventoryItemAttributeResponse,
+  b: InventoryItemAttributeResponse
+) => {
+  const aName = a.attribute.name.toLowerCase()
+  const bName = b.attribute.name.toLowerCase()
+  if (aName < bName) {
+    return -1
+  }
+  if (aName > bName) {
+    return 1
+  }
+  return 0
+}
+
+const RenderAttributeChips = (
   attributes?: InventoryItemAttributeResponse[]
 ) => {
-  return attributes?.map((itemAttribute, i) => {
+  const config = useAppSelector((state) => state.config)
+  const defaultAttrs = config.defaultAttributes
+  // console.log(defaultAttrs)
+  if (!attributes) {
+    return null
+  }
+
+  attributes.sort(attributeComparator)
+  // first get all the default attributes
+  // then get all the rest
+  const defaults: InventoryItemAttributeResponse[] = []
+  const nonDefaults: InventoryItemAttributeResponse[] = []
+  attributes.forEach((attr) => {
+    if (
+      defaultAttrs?.find(
+        (defaultAttr) => defaultAttr._id === attr.attribute._id
+      )
+    ) {
+      defaults.push(attr)
+    } else {
+      nonDefaults.push(attr)
+    }
+  })
+
+  const sortedAttrs = [...defaults, ...nonDefaults]
+
+  // const sortedAttributes: InventoryItemAttributeResponse[] = []
+  // const defaultSortedAttrs = attributes
+  //   .filter((attr) =>
+  //     defaultAttrs?.find((defaultAttr) => {
+  //       return defaultAttr._id === attr.attribute._id
+  //     })
+  //   )
+  //   .sort(attributeComparator)
+
+  return sortedAttrs.map((itemAttribute, i) => {
     // attributes that are strings or numbers show the attribute name
     // attributes that are list types do not
 
@@ -32,4 +83,4 @@ const renderAttributeChips = (
   })
 }
 
-export default renderAttributeChips
+export default RenderAttributeChips
