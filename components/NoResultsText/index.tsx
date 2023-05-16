4b06006349a9ba387b7da1057a6cc47595f8971b
Andrew Rutter <andlrutt@vols.utk.edu>
2023-05-16 18:49:10 -0400

    component

---
+++ components/NoResultsText/index.tsx
@@ -0,0 +1,12 @@
+import { Typography } from '@mui/material'
+
+export default function NoResultsText() {
+  return (
+    <>
+      <Typography>No results found</Typography>
+      <Typography>
+        Try adjusting your search or filter to find what you're looking for.
+      </Typography>
+    </>
+  )
+}
