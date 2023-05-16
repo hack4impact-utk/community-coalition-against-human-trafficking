ae350243792d4dc71872e421b18a025a51ede4a9
Zavier Miller <zavierjmiller@gmail.com>
2023-05-16 15:33:02 -0400

    add deterministic attribute ordering

--- utils/renderAttributeChips.tsx
+++ utils/renderAttributeChips.tsx
@@ -1,60 +1,24 @@
 import { Chip } from '@mui/material'
+import React from 'react'
 import { useAppSelector } from 'store'
 import getContrastYIQ from './getContrastYIQ'
+import { sortItemAttributes } from './sortAttributes'
 import { InventoryItemAttributeResponse } from './types'
-
-const attributeComparator = (
-  a: InventoryItemAttributeResponse,
-  b: InventoryItemAttributeResponse
-) => {
-  const aName = a.attribute.name.toLowerCase()
-  const bName = b.attribute.name.toLowerCase()
-  if (aName < bName) {
-    return -1
-  }
-  if (aName > bName) {
-    return 1
-  }
-  return 0
-}
 
 const RenderAttributeChips = (
   attributes?: InventoryItemAttributeResponse[]
 ) => {
   const config = useAppSelector((state) => state.config)
   const defaultAttrs = config.defaultAttributes
-  // console.log(defaultAttrs)
+
+  const sortedAttrs = React.useMemo(
+    () => sortItemAttributes(attributes || [], defaultAttrs),
+    [attributes, defaultAttrs]
+  )
+
   if (!attributes) {
     return null
   }
-
-  attributes.sort(attributeComparator)
-  // first get all the default attributes
-  // then get all the rest
-  const defaults: InventoryItemAttributeResponse[] = []
-  const nonDefaults: InventoryItemAttributeResponse[] = []
-  attributes.forEach((attr) => {
-    if (
-      defaultAttrs?.find(
-        (defaultAttr) => defaultAttr._id === attr.attribute._id
-      )
-    ) {
-      defaults.push(attr)
-    } else {
-      nonDefaults.push(attr)
-    }
-  })
-
-  const sortedAttrs = [...defaults, ...nonDefaults]
-
-  // const sortedAttributes: InventoryItemAttributeResponse[] = []
-  // const defaultSortedAttrs = attributes
-  //   .filter((attr) =>
-  //     defaultAttrs?.find((defaultAttr) => {
-  //       return defaultAttr._id === attr.attribute._id
-  //     })
-  //   )
-  //   .sort(attributeComparator)
 
   return sortedAttrs.map((itemAttribute, i) => {
     // attributes that are strings or numbers show the attribute name
