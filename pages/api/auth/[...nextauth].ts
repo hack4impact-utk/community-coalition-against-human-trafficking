8ccd2c0e6f2c03d7733ea882cc115c69629515fd
Zavier Miller <zavierjmiller@gmail.com>
2023-05-18 15:33:54 -0400

    remove gmail requirement

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -22,7 +22,7 @@ export const authOptions: NextAuthOptions = {
     async signIn({ account, profile }) {
       // Ensure that only users with the proper email domain may say in
       if (account?.provider === 'google') {
-        return profile?.email_verified && profile?.email?.endsWith('@gmail.com')
+        return profile?.email_verified
       }
       return true
     },
